69b9b46bc5cbb62db1d6d2372ba5bb7d1fff46e5
RavinduThilinaka <ravindu2001thilinaka@gmail.com>
2025-06-06 08:59:19 +0530

    create about section

--- pet/src/App.jsx
+++ pet/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './components/home/Home';
 import Service from './components/services/Service';
 import Footer from './components/footer/Footer';
 import Contact from './components/contact/Contact';
+import About from './components/about/About';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/service" element={<Service />} />
         <Route path="/footer" element={<Footer />} />
         <Route path="/contact" element={<Contact />} />
+         <Route path="/about" element={<About />} />
       </Routes>
     </Router>
   );
